bfb96a62aa1abbf78d550c3a391632d24feb5fe8
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-04 15:43:31 -0800

    add update and delete functions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,47 @@ class ListsController < ApplicationController
     render :index
   end
   def show
-  @list = List.find(params[:id])
-  render :show
-end
+    @list = List.find(params[:id])
+    render :show
+  end
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to  lists_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @list= List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  class ListsController < ApplicationController
+    def destroy
+      @list = List.find(params[:id])
+      @list.destroy
+      redirect_to lists_path
+    end
+  end
+
+private
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
