45b6d3277401177735332ca8286c032dcc943b44
Luke Philips and Tracie Weitzman <luke+tracie@>
2017-02-06 15:11:57 -0800

    add scopes for doneness to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :list
-
   validates :description, :presence => true
 
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
+  scope(:is_done, -> do
+    where({:done => true})
+  end)
 end
